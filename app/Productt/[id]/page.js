6496e336597e8d6b135b4bec3e72d01901471cc3
joy11122 <joyhaldar112233@gmail.com>
2024-07-15 16:20:12 -0700

    rtgf

--- app/Productt/[id]/page.js
+++ app/Productt/[id]/page.js
@@ -1,14 +1,13 @@
-"use client"
-import Quantity from "@/app/component/Quantity";
-import { useCart } from "@/src/context/cartContext";
+// pages/pageName.js
+import dynamic from 'next/dynamic';
 
-import React, { useState } from "react";
+// Import Quantity component dynamically with { ssr: false }
+const DynamicQuantity = dynamic(() => import('@/app/component/Quantity'), { ssr: false });
 
 const Page = ({ params }) => {
   const { product, handleAddToCart, amount } = useCart();
   const s = product.find((elm) => elm.id === params.id);
 
-  // Check if s or s.color exists before using useState
   const initialColor = s && s.color && s.color.length > 0 ? s.color[0] : null;
   const [color, setColor] = useState(initialColor);
 
@@ -17,7 +16,7 @@ const Page = ({ params }) => {
       <div className="container mt-3 color-section">
         <div className="row g-3">
           <div className="col-md-4 col-lg-4 col-sm-4">
-            <img className="w-100" style={{ height: 350 }} src={`../${s.image}`} alt="j" />
+            <img className="w-100" style={{ height: 350 }} src={`../${s.image}`} alt="Product" />
           </div>
           <div className="col-md-8 col-sm-8 col-lg-8">
             <div className="content ms-2 pt-2 pb-2">
@@ -29,17 +28,27 @@ const Page = ({ params }) => {
               </div>
               <p>{s.description}</p>
               <h6>
-                <span className="text-primary me-2">Prime</span>FREE Delivery by Monday October <span className="fw-bold">24</span>
+                <span className="text-primary me-2">Prime</span>FREE Delivery by Monday October{' '}
+                <span className="fw-bold">24</span>
               </h6>
-              <p>Only <span className="text-danger fw-bold">{s.total}</span> Left On Stock</p>
+              <p>
+                Only <span className="text-danger fw-bold">{s.total}</span> Left On Stock
+              </p>
 
               {s.color.map((elm, i) => (
-                <button style={{ backgroundColor: elm }} key={i} onClick={() => setColor(elm)}>
-                  {elm === color ? "+" : null}
+                <button
+                  style={{ backgroundColor: elm }}
+                  key={i}
+                  onClick={() => setColor(elm)}
+                >
+                  {elm === color ? '+' : null}
                 </button>
               ))}
-              <Quantity total={s.total} />
-              <button className="btn btn-info rounded-0" onClick={() => handleAddToCart(s.id, color, amount, product)}>
+              <DynamicQuantity total={s.total} />
+              <button
+                className="btn btn-info rounded-0"
+                onClick={() => handleAddToCart(s.id, color, amount, product)}
+              >
                 Add To Cart
               </button>
             </div>
