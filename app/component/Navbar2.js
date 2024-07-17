1a27ea28853375e73bd81c4794fb61dc5d302590
joy11122 <119776174+joy11122@users.noreply.github.com>
2024-07-17 15:11:10 +0600

    Update Navbar2.js

--- app/component/Navbar2.js
+++ app/component/Navbar2.js
@@ -6,35 +6,50 @@ import React from "react";
 
 const Navbar2 = () => {
   const { CartItem } = useCart();
- 
+
   return (
     <>
-
- <nav className="navbar ps-5 pe-5 bg-primary navbar-expand-sm navbar-dark fixed-top">
-  <a className="navbar-brand fw-bold " href="/">Daraz.com</a>
-  <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
-  <div className="collapse navbar-collapse" id="collapsibleNavId">
-    <ul className="navbar-nav me-auto gap-3">
- <Link className="nav-link text-black" href="/">Home</Link>
- <Link className="nav-link text-black" href="About">About</Link>
- <Link className="nav-link text-black" href="/Contact">Contact</Link>
- <Link className="nav-link text-black" href="/Product">Buy-Product</Link>
- <Link className="nav-link text-black" href="/Productt">Filter-product</Link>
-
-
-    </ul>
-    <form className="d-flex my-2 my-lg-0">
-      <input className="form-control me-sm-2" type="text" placeholder="Search" />
-      <button className="btn btn-success my-2 my-sm-0" type="submit">
-        Search
-      </button>
-    </form>
-  </div>
-</nav>
-
-
-    
-    
+      <nav className="navbar ps-5 pe-5 bg-primary navbar-expand-sm navbar-dark fixed-top">
+        <a className="navbar-brand fw-bold" href="/">Daraz.com</a>
+        <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavId">
+          <ul className="navbar-nav me-auto gap-3">
+            <li className="nav-item">
+              <Link className="nav-link text-black" href="/" passHref>
+                <a>Home</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-black" href="/About" passHref>
+                <a>About</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-black" href="/Contact" passHref>
+                <a>Contact</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-black" href="/Product" passHref>
+                <a>Buy-Product</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-black" href="/Productt" passHref>
+                <a>Filter-product</a>
+              </Link>
+            </li>
+          </ul>
+          <form className="d-flex my-2 my-lg-0">
+            <input className="form-control me-sm-2" type="text" placeholder="Search" />
+            <button className="btn btn-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
     </>
   );
 };
