529fe5e42ff4cc439d0eb0ae126a84c92a19f258
joy11122 <119776174+joy11122@users.noreply.github.com>
2024-07-17 15:38:56 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,28 +10,25 @@ import { CartProvider } from "@/src/context/cartContext";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head>
-    <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-          />
-          <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"
-            integrity="sha384-IQsoLXl5PILFhosVNubq7ALX+6IomF2Mx03p4JOa5f2lLkCVk6Mggw5UsvFYKX5N"
-            crossOrigin="anonymous"
-            defer
-          ></script>
-          <script
-            src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.0-beta3/js/bootstrap.min.js"
-            integrity="sha384-j0P37zC1cK1s7E80tqN/VL0n6QK9K94x8KEv9N1ks2EzH0KQjDzq4LDwi56BRz9K"
-            crossOrigin="anonymous"
-            defer
-          ></script>
-    </Head>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"
+          integrity="sha384-IQsoLXl5PILFhosVNubq7ALX+6IomF2Mx03p4JOa5f2lLkCVk6Mggw5UsvFYKX5N"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0.0-beta3/js/bootstrap.min.js"
+          integrity="sha384-j0P37zC1cK1s7E80tqN/VL0n6QK9K94x8KEv9N1ks2EzH0KQjDzq4LDwi56BRz9K"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body className="bg">
         <CartProvider>
           <Navbar2 />
-
           {children}
           <Fotter />
         </CartProvider>
